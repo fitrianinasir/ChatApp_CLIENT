baadf208ecf13492f2f11f55f75e2d136d567b3f
fitrianinasir <fitrianinasir8@gmail.com>
2020-07-01 00:02:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+# PROJECT INFO
+
+### 
+
+A Realtime Chat App using 
+- ReactJS for the client side 
+- Node.js and Socket.io for the server side
+
+~thanks to javascript mastery
+### 
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Join from './Components/Join/Join'
+import Chat from './Components/Chat/Chat'
+
+function App() {
+  return (
+    <Router>
+      <Route exact path="/" component={Join} />
+      <Route exact path="/chat" component={Chat}/>
+    </Router>
+  )
+}
+
+export default App
